2ae1beecdd56ac34367ccc114eada6bcf7d7086a
Moses Bomera <mosesbomer@gmail.com>
2020-07-12 11:31:45 +0300

    Add scripts.js

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () =>{
+  // Add margin-top
+  // document.querySelector('.anchors a').onclick = (e) => {
+  //   var element = e.target;
+  //   var a_attribute = element.getAttribute('href');
+  //   const link_element = document.querySelector(a_attribute);
+  //   link_element.style.marginBottom= "110px";
+  //   link_element.style.display = "block";
+  // };
+});
